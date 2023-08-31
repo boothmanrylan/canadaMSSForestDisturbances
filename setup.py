9a5a8c5ebb0bf8f51e9bc40d66d84a8bf755fbef
Rylan Boothman <boothmanrylan@gmail.com>
2023-08-31 11:57:46 -0400

    add setup.py for dataflow

---
+++ setup.py
@@ -0,0 +1,15 @@
+""" Setup script for Dataflow
+"""
+
+from setuptools import setup
+
+setup(
+    name="mssforestdisturbances",
+    url="https://github.com/boothmanrylan/canadaMSSForestDisturbances/tree/main/",
+    packages=["mss_forest_disturbances"],
+    install_requires=[
+        "apache-beam[gcp]==2.46.0",
+        "earthengine-api==0.1.358",
+        "tensorflow==2.12.0",
+    ],
+)
