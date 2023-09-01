52ab55933f1430938ffb837917d175a01ba6ed94
Rylan Boothman <boothmanrylan@gmail.com>
2023-09-01 12:24:45 -0400

    bump apache beam version in setup.py to 2.49.0

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ setup(
     url="https://github.com/boothmanrylan/canadaMSSForestDisturbances/tree/main/",
     packages=["mss_forest_disturbances"],
     install_requires=[
-        "apache-beam[gcp]==2.46.0",
+        "apache-beam[gcp]==2.49.0",
         "earthengine-api==0.1.358",
         "tensorflow==2.12.0",
         "msslib"
